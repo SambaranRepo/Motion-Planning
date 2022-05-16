6888577f0022a7bc514c874550ccd0ea8cf01e3e
SambaranRepo <sambaranghosal@gmail.com>
2022-05-16 01:08:05 -0700

    Implementation of RTAA*, ARA* and RRT based search and sampling based motion planning algorithms to catch a moving target

--- modify_map.py
+++ modify_map.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('map', help='Enter the map you want to modify among these two : \n 1. Map1 ---> 1 \n 2. Map7 ---> 7', type=int)
+args = parser.parse_args()
+map_no = args.map
+assert isinstance(map_no, int) and map_no in [1,7]
 
 
-envmap = np.loadtxt('./maps/map7.txt')
+envmap = np.loadtxt(f'./maps/map{map_no}.txt')
 x_size,y_size = envmap.shape
 
-res = 10
+res = 10 if map_no == 7 else 5
 newenvmap = np.zeros((x_size // res + 1, y_size //res + 1))
 for i in range(0, x_size, res):
     for j in range(0, y_size, res):
@@ -22,4 +28,4 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')  
 plt.show(block = True)
 
-np.savetxt("./maps/map7_modified.txt", newenvmap,fmt = '%d' )
+np.savetxt(f"./maps/map{map_no}_modified.txt", newenvmap,fmt = '%d' )
